909ac6fb6e55aa5fa9e8a90c4f7a2d981dbd1c3e
Federico Gobbo <federico.gobbo.2@studenti.unipd.it>
2017-03-26 22:45:36 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase=nil)
-  if phrase
-    puts phrase
-    phrase
-  else
-    puts "Squawk!"
-    "Squawk!"
-  end
+def parrot(phrase="Squawk!")
+  puts phrase
+  phrase
 end
