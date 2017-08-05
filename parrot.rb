c757ce5ba7dee2c5128bcbc2b65d8ff499f6f0ab
Brennan Brown <brennan.brown@edu.sait.ca>
2017-08-05 05:22:05 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+# This lab is to demonstrate control of the return value of a method.
+
+# Method has an argument that has a default value.
+def parrot(squawk = "Squawk!")
+  # Returns the argument on the CLI.
+  puts "#{squawk}"
+  # Returns the argument to be used by other methods.
+  squawk
+end
