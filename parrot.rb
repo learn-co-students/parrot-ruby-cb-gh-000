7c0eb16b305d3a2f9758835f23f48b1fc7f12a2b
norman lewis <takialog@gmail.com>
2018-02-27 08:31:01 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,6 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(name="Squawk!")
+  puts "#{name}"
+  name
+end
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
