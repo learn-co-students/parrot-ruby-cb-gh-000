d523fb0bc7e3ba3c17dd15371d0871ba991ffe4c
Pablo Peña <pablo@makeitworth.com.mx>
2018-05-13 19:55:00 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(talk=nil)
-  if talk
-    puts talk
-    talk
-  else
-    puts "Squawk!"
-    talk = "Squawk!"
-  end
+def parrot(phrase = "Squawk!")
+  puts phrase
+  return phrase
 end
