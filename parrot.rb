0efda56577becaac97eedb7c282175b6e6ca6778
Ruthvik Vegunta <ruthvikvegunta2@gmail.com>
2018-12-20 09:25:42 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(name = nil)
+  if name == nil
+    puts "Squawk!"
+    return "Squawk!"
+  else
+    puts "Pretty bird!"
+    return "Pretty bird!"
+  end
+end
